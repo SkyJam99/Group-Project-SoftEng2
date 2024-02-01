56273a6af723d5e3833b417dab2edeb45192bdbe
SkyJam99 <125305002+SkyJam99@users.noreply.github.com>
2024-02-01 13:26:02 -0400

    Added GUI!

--- SoftEng2Project/src/App.java
+++ SoftEng2Project/src/App.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
     }
 }
