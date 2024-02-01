fb567d5f05ba7fb1e620000c26e7a5fe4501db5a
SkyJam99 <125305002+SkyJam99@users.noreply.github.com>
2024-02-01 13:33:57 -0400

    Test 2

--- SoftEng2Project/src/App.java
+++ SoftEng2Project/src/App.java
@@ -5,7 +5,7 @@ public class App {
         System.out.println("Hello, World!");
         JFrame frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(300,300);
+        frame.setSize(600,600);
         JButton button = new JButton("Press");
         frame.getContentPane().add(button); // Adds Button to content pane of frame
         frame.setVisible(true);
