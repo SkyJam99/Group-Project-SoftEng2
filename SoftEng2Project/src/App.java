cd3fc5ed4a796bf668dd3b71b74f08a762ec0fb9
SkyJam99 <125305002+SkyJam99@users.noreply.github.com>
2024-02-01 14:11:46 -0400

    Added more detail to prototype GUI

--- SoftEng2Project/src/App.java
+++ SoftEng2Project/src/App.java
@@ -1,13 +1,44 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        JFrame frame = new JFrame("My First GUI");
+        //Creating the Frame
+        JFrame frame = new JFrame("Quiz Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(600,600);
-        JButton button = new JButton("Press");
-        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setSize(500, 500);
+
+        //Creating the MenuBar and adding components
+        JMenuBar mb = new JMenuBar();
+        JMenu m1 = new JMenu("Choose Category");
+        JMenu m2 = new JMenu("Help");
+        mb.add(m1);
+        mb.add(m2);
+        JMenuItem m11 = new JMenuItem("Science");
+        JMenuItem m22 = new JMenuItem("History");
+        JMenuItem m33 = new JMenuItem("Entertainment");
+        m1.add(m11);
+        m1.add(m22);
+        m1.add(m33);
+
+        //Creating the panel at bottom and adding components
+        JPanel panel = new JPanel(); // the panel is not visible in output
+        JLabel label = new JLabel("Enter Text");
+        JTextField tf = new JTextField(10); // accepts upto 10 characters
+        JButton send = new JButton("Send");
+        JButton reset = new JButton("Reset");
+        panel.add(label); // Components Added using Flow Layout
+        panel.add(tf);
+        panel.add(send);
+        panel.add(reset);
+
+        // Text Area at the Center
+        JPanel panel2 = new JPanel();
+
+        //Adding Components to the frame.
+        frame.getContentPane().add(BorderLayout.SOUTH, panel);
+        frame.getContentPane().add(BorderLayout.NORTH, mb);
+        frame.getContentPane().add(BorderLayout.CENTER, panel2);
         frame.setVisible(true);
     }
 }
