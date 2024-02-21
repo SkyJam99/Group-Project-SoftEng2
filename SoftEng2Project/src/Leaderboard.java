8262312c11ffa2f4c961670a45926dd083269e40
SkyJam99 <125305002+SkyJam99@users.noreply.github.com>
2024-02-21 14:51:36 -0400

    Added logic for getting and updating scores

--- SoftEng2Project/src/Leaderboard.java
+++ SoftEng2Project/src/Leaderboard.java
@@ -18,9 +18,9 @@ public class Leaderboard {
                 tempScore = line.split(" ");
                 scores[i][0] = tempScore[0];
                 scores[i][1] = tempScore[1];
-                System.out.println(line);
+                /*System.out.println(line); //Testing code
                 System.out.println(scores[i][0]);
-                System.out.println(scores[i][1]);
+                System.out.println(scores[i][1]);*/
             }
         } catch (IOException e) {
             System.out.println("Error retrieving scores");
@@ -34,17 +34,44 @@ public class Leaderboard {
     //and place the new score in the correct placement
     //while removing score #11. Ties will result in the older score being ranked higher
     public void addNewScore(String name, int score) {
+        String[][] oldScores = new String[10][2];
+        String[][] newScores = new String[10][2];
+        String finalScores = "";
+        boolean placedNewScore = false;
+        int tempScore;
+
         //Get existing scores
+        oldScores = getScores();
 
         //Use insertion sort to place the new score in a sorted array
+        for(int i = 0; i < 10; i++) {
+            try{ //Store integer value of score
+                tempScore = Integer.parseInt(oldScores[i][1]);
+            } catch (NumberFormatException ex) {
+                System.out.println("Error converting score to integer for sorting");
+                break;
+            }
 
-        //Erase the information in the old scores.txt file
+            if(placedNewScore) { //If we've placed the new score we can just go through the rest of the old scores
+                newScores[i][0] = oldScores[i-1][0];
+                newScores[i][1] = oldScores[i-1][1];
+            } else if(score > tempScore) { //This is what places the new score
+                placedNewScore = true;
+                newScores[i][0] = name;
+                newScores[i][1] = String.valueOf(score);
+            } else { //If the current oldScore is greater than the new score
+                newScores[i][0] = oldScores[i][0];
+                newScores[i][1] = oldScores[i][1];
+            }
+            System.out.println(newScores[i][0]);
+            System.out.println(newScores[i][1]);
+            //Create string containing all the new scores
+            finalScores = finalScores + newScores[i][0] + " " + newScores[i][1] + "\n";
+        }
 
-        //Write the top 10 scores to the scores.txt file
-
-        //Return true / false 
-        try(FileWriter writer = new FileWriter(FILE_PATH, true)){
-            writer.write("test\n");
+        //Overwrite the top 10 scores in the scores.txt file
+        try(FileWriter writer = new FileWriter(FILE_PATH, false)){
+            writer.write(finalScores);
         } catch (IOException e) {
             System.out.println("Error while writing to file");
         }
@@ -52,8 +79,8 @@ public class Leaderboard {
 
     public static void main(String[] args) {
         Leaderboard test = new Leaderboard();
-        //test.addNewScore("Jam", 1);
-        test.getScores();
+        test.addNewScore("Riley", 1);
+        //test.getScores();
     }
 }
 
