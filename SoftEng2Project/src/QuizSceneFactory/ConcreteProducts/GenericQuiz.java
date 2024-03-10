b8579a7c3b4b481f4e0c3ec98e11bbe811519ef8
SkyJam99 <125305002+SkyJam99@users.noreply.github.com>
2024-03-10 15:42:19 -0300

    Shuffled options

--- SoftEng2Project/src/QuizSceneFactory/ConcreteProducts/GenericQuiz.java
+++ SoftEng2Project/src/QuizSceneFactory/ConcreteProducts/GenericQuiz.java
@@ -5,6 +5,10 @@ import java.awt.*;
 import java.awt.event.*;
 import app.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GenericQuiz {
     public static JPanel quizConstructor(String category, int questionNum, String questionText, String optionA, String optionB, String optionC, String optionD, String correctOption) {
         JPanel panel = new JPanel();
@@ -12,16 +16,23 @@ public class GenericQuiz {
         JLabel panelNameLabel = new JLabel(panelName);
         panel.add(panelNameLabel);
 
+        List<String> shuffledOptions = new ArrayList<>(List.of(optionA, optionB, optionC, optionD));
+        Collections.shuffle(shuffledOptions);
+        String opA = shuffledOptions.get(0);
+        String opB = shuffledOptions.get(1);
+        String opC = shuffledOptions.get(2);
+        String opD = shuffledOptions.get(3);
+
         JPanel layoutPanel = new JPanel();
         layoutPanel.setLayout(new BoxLayout(layoutPanel, BoxLayout.Y_AXIS));
 
         JLabel questionTextLabel = new JLabel(questionText);
         panel.add(questionTextLabel);
 
-        JRadioButton optionAButton = new JRadioButton(optionA);
-        JRadioButton optionBButton = new JRadioButton(optionB);
-        JRadioButton optionCButton = new JRadioButton(optionC);
-        JRadioButton optionDButton = new JRadioButton(optionD);
+        JRadioButton optionAButton = new JRadioButton(opA);
+        JRadioButton optionBButton = new JRadioButton(opB);
+        JRadioButton optionCButton = new JRadioButton(opC);
+        JRadioButton optionDButton = new JRadioButton(opD);
 
         ButtonGroup group = new ButtonGroup();
         group.add(optionAButton);
@@ -46,13 +57,13 @@ public class GenericQuiz {
 
                 String selectedOption;
                 if(optionAButton.isSelected()) {
-                    selectedOption = optionA;
+                    selectedOption = opA;
                 } else if(optionBButton.isSelected()) {
-                    selectedOption = optionB;
+                    selectedOption = opB;
                 } else if(optionCButton.isSelected()) {
-                    selectedOption = optionC;
+                    selectedOption = opC;
                 } else {
-                    selectedOption = optionD;
+                    selectedOption = opD;
                 }
 
                 if(selectedOption.equals(correctOption)) {
