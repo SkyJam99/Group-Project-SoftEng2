dcb9d4ef30c964b3512905fc8e88cb1803671011
SkyJam99 <125305002+SkyJam99@users.noreply.github.com>
2024-03-17 20:58:16 -0300

    Added comments to context class

--- SoftEng2Project/src/QuizStrategy/Context.java
+++ SoftEng2Project/src/QuizStrategy/Context.java
@@ -2,17 +2,51 @@ package QuizStrategy;
 
 import javax.swing.JPanel;
 
+/**
+ * Class Name: Context
+ * Purpose: Defines the context for the Strategy pattern.
+ * Usage: Used to execute a strategy for creating quiz scenes.
+ */
+
 public class Context {
     private Quiz _strategy;
 
+    /**
+     * Method Name: Context
+     * Purpose: Constructor for the Context class.
+     * Parameters:
+     *     - strategy (Quiz): The strategy to be executed.
+     * Returns: None
+     */
     public Context(Quiz strategy) {
         this._strategy = strategy;
     }
 
+    /**
+     * Method Name: setStrategy
+     * Purpose: Sets the strategy to be executed.
+     * Parameters:
+     *     - strategy (Quiz): The strategy to be executed.
+     * Returns: None
+     */
     public void setStrategy(Quiz strategy) {
         this._strategy = strategy;
     }
 
+    /**
+     * Method Name: executeStrategy
+     * Purpose: Executes the strategy to create a quiz scene.
+     * Parameters:
+     *     - category (String): The category of the question.
+     *     - questionNum (int): The question number within the category.
+     *     - questionText (String): The text of the question
+     *     - optionA (String): Text for option A.
+     *     - optionB (String): Text for option B.
+     *     - optionC (String): Text for option C.
+     *     - optionD (String): Text for option D.
+     *     - correctOption (String): The correct option.
+     * Returns: JPanel with the question and options.
+     */
     public JPanel executeStrategy(String category, int questionNum, String questionText, String optionA, String optionB, String optionC, String optionD, String correctOption) { 
         return this._strategy.quizConstructor(category, questionNum, questionText, optionA, optionB, optionC, optionD, correctOption);
     }
